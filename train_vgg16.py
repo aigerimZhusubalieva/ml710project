abecc590a8b34e2f3e4c67d686d733bf11584b58
Mena Attia <mena.attia.ma@gmail.com>
2025-04-13 14:19:01 +0400

    add validation code

--- train_vgg16.py
+++ train_vgg16.py
@@ -8,6 +8,29 @@ import os
 import wandb
 import pynvml
 
+def evaluate(model_engine, dataloader, criterion):
+    model_engine.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images, labels = images.to(model_engine.local_rank), labels.to(model_engine.local_rank)
+            if model_engine.fp16_enabled():
+                images = images.half()
+            outputs = model_engine(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item() * images.size(0)
+
+            _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)
+
+    val_loss /= len(dataloader.dataset)
+    accuracy = correct / total
+    model_engine.train()
+    return val_loss, accuracy
 
 def main():
     parser = argparse.ArgumentParser()
@@ -24,6 +47,7 @@ def main():
     run = wandb.init(
         entity="ML701",   
         project="ML710_Project",  # <<<< change this to your project name
+        name="ddp-baseline-run1",
         config={
             "learning_rate": args.lr,
             "batch_size": args.batch_size,
@@ -47,17 +71,20 @@ def main():
         transforms.Resize((224, 224)),
         transforms.ToTensor()
     ])
-    trainset = torchvision.datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform)
 
+    trainset = torchvision.datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform)
     # Use DistributedSampler
     train_sampler = DistributedSampler(trainset)
-
     # Create DataLoader with the sampler
     trainloader = DataLoader(trainset, batch_size=args.batch_size, sampler=train_sampler)
 
+    valset = torchvision.datasets.ImageFolder(os.path.join(args.data_path, 'validation'), transform=transform)
+    val_sampler = DistributedSampler(valset)
+    valloader = DataLoader(valset, batch_size=args.batch_size, sampler=val_sampler) 
+
     model = torchvision.models.vgg16(weights='IMAGENET1K_V1')
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     model_engine, optimizer, _, _ = deepspeed.initialize(args=args, model=model, optimizer=optimizer)
 
@@ -85,6 +112,15 @@ def main():
                 "gpu_memory_allocated_MB": memory_allocated,
                 "gpu_memory_reserved_MB": memory_reserved,
                 "gpu_utilization_percent": utilization
+            })
+        
+        val_loss, val_acc = evaluate(model_engine, valloader, criterion)
+        print("validation_loss", val_loss)
+        if model_engine.local_rank == 0:
+            wandb.log({
+                "val_loss": val_loss,
+                "val_accuracy": val_acc,
+                "epoch": epoch
             })
 
     if model_engine.local_rank == 0:
